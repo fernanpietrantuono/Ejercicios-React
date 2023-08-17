d2a9173be0fa568b34eeb7cba6c5fd7c848eb01f
Fernan Pietrantuono <fernanpietrantuono@gmail.com>
2023-08-17 09:06:28 -0300

    Primer Commit

---
+++ index/src/Components/Public/Ejercicio 1.js
@@ -0,0 +1,10 @@
+export const Ejemplo = () => {
+    const titulo = "Hello Dog";
+    const subtitulo = "Subtítulo";
+    return(
+        <div>
+            <h1>{titulo}</h1>
+            <h2>{subtitulo}</h2>
+        </div>
+    );
+};
